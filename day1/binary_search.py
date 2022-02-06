123ce60cd665e5f17519cb59ad1fe46179a9c944
rmanaem <rmanaem@protonmail.ch>
2022-02-06 18:25:38 -0500

    Implemented the answer for binary_search.py

--- day1/binary_search.py
+++ day1/binary_search.py
@@ -11,3 +11,25 @@ Input: nums = [-1, 0, 3, 5, 9, 12], target = 2
 Output: -1
 Explanation: 2 does not exist in nums so return -1 
 """
+
+
+def search(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    s = 0
+    e = len(nums)-1
+    while s <= e:
+        m = s + (e - s)//2
+        if target == nums[m]:
+            return m
+        if target > nums[m]:
+            s = m + 1
+        else:
+            e = m - 1
+    return -1
+
+
+print(search([-1, 0, 3, 5, 9, 12], 1))
