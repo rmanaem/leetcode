79e1561f0cc5d81cbadd2130fbc45d65652a7b76
rmanaem <rmanaem@protonmail.ch>
2022-02-07 11:25:12 -0500

    Implemented the answer for reverse_words_in_string.py

--- day4/reverse_words_in_string.py
+++ day4/reverse_words_in_string.py
@@ -7,3 +7,35 @@ Example 1:
 Input: s = "God Ding"
 Output: "doG gniD"
 """
+
+
+from re import S
+
+
+def reverseWords(s):
+    temp = ''
+    s = s.split(' ')
+    for i in range(len(s)):
+        ls = []
+        for j in range(len(s[i])):
+            ls.append(s[i][j])
+        s[i] = ls
+
+    for w in s:
+        n = len(w)
+        for i in range(n//2):
+            temp = w[i]
+            w[i] = w[n-1-i]
+            w[n-1-i] = temp
+    rev = ''
+    for i in range(len(s)):
+        temp = ''
+        for c in s[i]:
+            temp += c
+        rev += temp + ' '
+    rev = rev[:len(rev)-1]
+    print(rev)
+
+
+reverseWords('Let\'s take LeetCode contest')
+reverseWords('God Ding')
