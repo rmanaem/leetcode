1ba5ed9a19a127fe1fe4e73df596f96c719376c8
rmanaem <rmanaem@protonmail.ch>
2022-02-07 12:26:56 -0500

    Added the link for reverse_words_in_string.cpp

--- day4/reverse_words_in_string.cpp
+++ day4/reverse_words_in_string.cpp
@@ -1,4 +1,5 @@
-/* Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+/* Link: https://leetcode.com/problems/reverse-words-in-a-string-iii/
+Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
 Example 1:
 Input: s = "Let's take LeetCode contest"
 Output: "s'teL ekat edoCteeL tsetnoc"
