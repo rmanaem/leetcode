b0363f3de830dbf4d125ad6e42202f10cc9f869d
rmanaem <rmanaem@protonmail.ch>
2022-02-06 20:07:19 -0500

    Implemented the answer for squared_sorted_array.cpp

--- day2/squared_sorted_array.cpp
+++ day2/squared_sorted_array.cpp
@@ -7,3 +7,39 @@ After sorting, it becomes [0,1,9,16,100].
 Example 2:
 Input: nums = [-7,-3,2,3,11]
 Output: [4,9,9,49,121] */
+
+// Solution using two pointers
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+
+std::vector<int> sortedSquare(std::vector<int> &nums)
+{
+    int n = 0;
+    int p = nums.size() - 1;
+    std::vector<int> v(nums.size());
+    for (int i = v.size() - 1; i >= 0; i--)
+    {
+        if (abs(nums[p]) > abs(nums[n]))
+        {
+            v[i] = nums[p] * nums[p];
+            p--;
+        }
+        else
+        {
+            v[i] = nums[n] * nums[n];
+            n++;
+        }
+    }
+    return v;
+}
+
+int main()
+{
+    std::vector<int> v{-7, -3, 2, 3, 11};
+    for (int i : sortedSquare(v))
+    {
+        std::cout << " " << i;
+    }
+    return 0;
+}
