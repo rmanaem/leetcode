e6364f3086aa15660c076cb9be5bd8acac1d1d53
rmanaem <rmanaem@protonmail.ch>
2022-02-07 12:33:30 -0500

    Added the link for squared_sorted_array.cpp

--- day2/squared_sorted_array.cpp
+++ day2/squared_sorted_array.cpp
@@ -1,4 +1,6 @@
-/* Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+/*
+Link: https://leetcode.com/problems/squares-of-a-sorted-array/
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
 Example 1:
 Input: nums = [-4,-1,0,3,10]
 Output: [0,1,9,16,100]
