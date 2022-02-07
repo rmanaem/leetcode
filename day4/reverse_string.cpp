580d52f6cc88ec6574c672edc61e8ef4ae3359c3
rmanaem <rmanaem@protonmail.ch>
2022-02-07 12:29:12 -0500

    Added the link for reverse_string.cpp

--- day4/reverse_string.cpp
+++ day4/reverse_string.cpp
@@ -1,4 +1,5 @@
-/* Write a function that reverses a string. The input string is given as an array of characters s.
+/* Link: https://leetcode.com/problems/reverse-string/
+ Write a function that reverses a string. The input string is given as an array of characters s.
 You must do this by modifying the input array in-place with O(1) extra memory.
 Example 1:
 Input: s = ["h","e","l","l","o"]
