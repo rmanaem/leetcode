2b76c75d6afb8af2bda2556c94005f72338196fd
rmanaem <rmanaem@protonmail.ch>
2022-02-07 12:32:00 -0500

    Added the link for rotate_array.py

--- day2/rotate_array.py
+++ day2/rotate_array.py
@@ -1,4 +1,5 @@
 """
+Link: https://leetcode.com/problems/rotate-array/
 Given an array, rotate the array to the right by k steps, where k is non-negative.
 Example 1:
 Input: nums = [1,2,3,4,5,6,7], k = 3
