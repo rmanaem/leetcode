eaf172e403ddebac335f36912da6416cfaf1c4b3
rmanaem <rmanaem@protonmail.ch>
2022-02-07 12:34:43 -0500

    Added the link for binary_search.py

--- day1/binary_search.py
+++ day1/binary_search.py
@@ -1,5 +1,6 @@
 """
- Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums.
+Link: https://leetcode.com/problems/binary-search/
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums.
 If target exists, then return its index. Otherwise, return -1.
 You must write an algorithm with O(log n) runtime complexity.
 Example 1:
