9532d03293725b1c7ebd291a6949c2b92eca994c
rmanaem <rmanaem@protonmail.ch>
2022-02-07 20:11:56 -0500

    Implemented the answer for merge_two_sorted_lists.cpp

--- day7/merge_two_sorted_lists.cpp
+++ day7/merge_two_sorted_lists.cpp
@@ -19,3 +19,31 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 }; */
+
+ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+{
+    if (list1 == nullptr & list2 == nullptr)
+    {
+        return list1;
+    }
+    if (list1 == nullptr)
+    {
+        return list2;
+    }
+    if (list2 == nullptr)
+    {
+        return list1;
+    }
+    ListNode *temp = nullptr;
+
+    if (list1->val > list2->val)
+    {
+        list2->next = mergeTwoLists(list1, list2->next);
+        return list2;
+    }
+    else
+    {
+        list1->next = mergeTwoLists(list1->next, list2);
+        return list1;
+    }
+}
