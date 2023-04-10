c90172da0fd02a1b075020a4a759bccfafa6d5bb
Arman <rmanaem@protonmail.ch>
2023-04-10 08:56:21 -0400

    Implemented solution for reverse_word_instring.cpp

--- day4/reverse_words_in_string.cpp
+++ day4/reverse_words_in_string.cpp
@@ -6,3 +6,48 @@ Output: "s'teL ekat edoCteeL tsetnoc"
 Example 1:
 Input: s = "God Ding"
 Output: "doG gniD" */
+
+#include <string>
+#include <iostream>
+
+char *reverseString(char s[], int n)
+{
+    char temp = 'a';
+    for (int i = 0; i < n / 2; i++)
+    {
+        temp = s[i];
+        s[i] = s[n - 1 - i];
+        s[n - 1 - i] = temp;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << s[i] << " ";
+    }
+    return s;
+}
+
+char *stringToArrary(char s[], int n)
+{
+    char *arr = new char[n];
+    int temp = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == ' ')
+        {
+        }
+    }
+}
+
+std::string reverseWords(std::string s)
+{
+    std::string s("s");
+    return s;
+}
+
+int main()
+{
+    std::string s = "allo";
+    char arr[] = "Let's go fuck";
+    std::cout << arr[1];
+    return 0;
+}
